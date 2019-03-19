b01f323f4ee415487e4d9f7dd75166976cd9b53f
Nick <raschyk1993@gmail.com>
2019-03-19 14:49:02 +0200

    thunk

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,9 @@
-import {createStore} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import rootReducers from '../reducers';
+import thunk from 'redux-thunk';
 
 const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(rootReducers, DevTools);
+const store = createStore(rootReducers, compose(applyMiddleware(thunk), DevTools));
 
 export default store;
